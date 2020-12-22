5733e1fc010b375bbafc9ebfa41e248845f945b3
Marcel Kulina <marcel.kulina@outlook.de>
2020-12-22 16:38:57 +0100

    no message

--- Core/GraphicsKit/Config.hxx
+++ Core/GraphicsKit/Config.hxx
@@ -6,7 +6,6 @@ constexpr uint8_t Contexts = 2;
 constexpr uint8_t CommandLists = 3;
 constexpr uint8_t CommandQueues = 4;
 
-
 constexpr uint8_t CommandListPre = 0;
 constexpr uint8_t CommandListMid = 1;
 constexpr uint8_t CommandListPost = 2;
